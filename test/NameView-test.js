2a047de70ca0b2e42b8548c3d01d550b09c133cd
Yuichi Tanikawa <t.yuichi111@gmail.com>
2016-09-01 20:14:49 +0900

    Fix max-len errors in lint

--- test/NameView-test.js
+++ test/NameView-test.js
@@ -25,14 +25,16 @@ describe('NameView', function () {
     });
 
     it('extracts English name from the string containing slash (1)', function () {
-      document.body.innerHTML = window.__html__['test/fixtures/h2-stardust-dragon-assult-mode.html'];
+      document.body.innerHTML =
+        window.__html__['test/fixtures/h2-stardust-dragon-assult-mode.html'];
       var $name = document.body.firstChild;
       var nameView = new NameView($name);
       expect(nameView).to.have.property('name', 'Stardust Dragon/Assault Mode');
     });
 
     it('extracts English name from the string containing slash (2)', function () {
-      document.body.innerHTML = window.__html__['test/fixtures/h2-ddd-duo-dawn-king-kali-yuga.html'];
+      document.body.innerHTML =
+        window.__html__['test/fixtures/h2-ddd-duo-dawn-king-kali-yuga.html'];
       var $name = document.body.firstChild;
       var nameView = new NameView($name);
       expect(nameView).to.have.property('name', 'D/D/D Duo-Dawn King Kali Yuga');
