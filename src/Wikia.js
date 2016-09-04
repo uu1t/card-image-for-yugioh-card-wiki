47fbe629479bd43a495cecfdc60abe1007a1a834
Yuichi Tanikawa <t.yuichi111@gmail.com>
2016-09-05 03:46:46 +0900

    Add and test Wikia#fetchContents()

--- src/Wikia.js
+++ src/Wikia.js
@@ -3,18 +3,36 @@
 function Wikia() {}
 
 Wikia.prototype.imageServingUrl = 'http://yugioh.wikia.com/api.php?action=imageserving&format=json&wisTitle=';
+Wikia.prototype.fetchContentsUrl = 'http://yugioh.wikia.com/api.php?format=json&action=query&prop=revisions&rvprop=content&titles=';
+
+Wikia.prototype.toJSON = function (response) {
+  return response.json();
+};
 
 Wikia.prototype.fetchImageUrl = function (name) {
   var url = this.imageServingUrl + encodeURI(name);
-  return fetch(url).then(function (response) {
-    return response.json();
-  }).then(function (json) {
-    return this.extractUrl(json);
-  }.bind(this));
+  return fetch(url).then(this.toJSON).then(this.extractUrl);
 };
 
 Wikia.prototype.extractUrl = function (json) {
   return json.image.imageserving;
+};
+
+Wikia.prototype.fetchContents = function (titles) {
+  var url = this.fetchContentsUrl + encodeURI(titles.join('|'));
+  return fetch(url).then(this.toJSON).then(this.parseContents);
+};
+
+Wikia.prototype.parseContents = function (json) {
+  var pages = json.query.pages;
+  for (var id in pages) {
+    if (Object.prototype.hasOwnProperty.call(pages, id)) {
+      pages[id].id = pages[id].pageid;
+      pages[id].content = pages[id].revisions[0]['*'];
+    }
+  }
+
+  return pages;
 };
 
 module.exports = Wikia;
--- test/Wikia-test.js
+++ test/Wikia-test.js
@@ -1,6 +1,10 @@
 var expect = require('chai').expect;
 var Wikia = require('../src/Wikia');
 
+function parseHTTPBinArg(arg, json) {
+  return json.args[arg];
+}
+
 describe('Wikia', function () {
   describe('#fetchImageUrl()', function () {
     it('fetches image url', function () {
@@ -17,13 +21,53 @@ describe('Wikia', function () {
     it('sends request with name', function () {
       var wikia = new Wikia();
       wikia.imageServingUrl = 'https://httpbin.org/get?wisTitle=';
-      wikia.extractUrl = function extractArgs(json) {
-        return json.args.wisTitle;
-      };
+      wikia.extractUrl = parseHTTPBinArg.bind(null, 'wisTitle');
 
       return wikia.fetchImageUrl('Blue-Eyes White Dragon').then(function (name) {
         expect(name).to.equal('Blue-Eyes White Dragon');
       });
+    });
+  });
+
+  describe('#fetchContents()', function () {
+    it('fetches contents', function () {
+      var wikia = new Wikia();
+
+      // See http://myjson.com/25v0k
+      wikia.fetchContentsUrl = 'https://api.myjson.com/bins/25v0k';
+
+      return wikia.fetchContents([]).then(function (contents) {
+        expect(contents).to.be.a('object');
+        Object.keys(contents).forEach(function (id) {
+          expect(contents[id]).to.contain.all.keys('id', 'title', 'content');
+        });
+      });
+    });
+
+    it('sends requests with titles delimited with |', function () {
+      var wikia = new Wikia();
+      wikia.fetchContentsUrl = 'https://httpbin.org/get?titles=';
+      wikia.parseContents = parseHTTPBinArg.bind(null, 'titles');
+
+      return wikia.fetchContents(['t1', 't2', 't3']).then(function (titles) {
+        expect(titles).to.equal('t1|t2|t3');
+      });
+    });
+  });
+
+  describe('#parseContents()', function () {
+    it('parses contents json', function () {
+      var wikia = new Wikia();
+      var json = require('./fixtures/contents-a-b-c.json');
+      var contents = wikia.parseContents(json);
+
+      expect(contents).to.have.all.keys('567356', '567360', '567361');
+      expect(contents[567356]).to.have.property('id', 567356);
+      expect(contents[567356]).to.have.property('content', 'Ａ－アサルト・コア');
+      expect(contents[567360]).to.have.property('id', 567360);
+      expect(contents[567360]).to.have.property('content', 'Ｂ－バスター・ドレイク');
+      expect(contents[567361]).to.have.property('id', 567361);
+      expect(contents[567361]).to.have.property('content', 'Ｃ－クラッシュ・ワイバーン');
     });
   });
 });
