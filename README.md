d85636cd49b9dcbad3ba21368d087fa5b23fa9d2
Yuichi Tanikawa <t.yuichi111@gmail.com>
2016-12-02 15:25:05 +0900

    Run test on Travis CI (#1)
    * Delete trailing commas
    
    * Run karma with Chrome on Travis CI
    
    * Install google-chrome-stable
    
    * Add build status badge

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 カード画像 for 遊戯王カードWiki
 ====
 
+[![Build Status](https://travis-ci.org/itiut/card-image-for-yugioh-card-wiki.svg?branch=master)](https://travis-ci.org/itiut/card-image-for-yugioh-card-wiki)
+
 > :flower_playing_cards: [遊戯王カードWiki](http://yugioh-wiki.net/)でカード画像を表示します
--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,23 @@
 module.exports = function (config) {
-  config.set({
+  var configuration = {
     basePath: '',
     frameworks: ['browserify', 'mocha'],
     files: [
       'test/*-test.js',
-      'test/fixtures/*.html',
+      'test/fixtures/*.html'
     ],
     exclude: [],
     preprocessors: {
       'test/*-test.js': ['browserify'],
-      'test/fixtures/*.html': ['html2js'],
+      'test/fixtures/*.html': ['html2js']
     },
     browserify: {
-      debug: true,
+      debug: true
     },
     clinet: {
       mocha: {
-        timeout: 5000,
-      },
+        timeout: 5000
+      }
     },
     reporters: ['mocha'],
     port: 9876,
@@ -25,7 +25,20 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeTravisCi: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
-    concurrency: Infinity,
-  });
+    concurrency: Infinity
+  };
+
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['ChromeTravisCi'];
+    configuration.singleRun = true;
+  }
+
+  config.set(configuration);
 };
