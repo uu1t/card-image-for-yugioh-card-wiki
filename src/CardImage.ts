ad77478897919152712eca6bef3e99f525548b4c
Yuichi Tanikawa <kojole.jp@gmail.com>
2018-08-20 14:44:21 +0900

    Build for firefox

--- src/CardImage.ts
+++ src/CardImage.ts
@@ -2,12 +2,18 @@ export default class CardImage {
   constructor(private url: string) {}
 
   public appendTo($el: Element) {
-    if (!$el.parentElement) {
+    if (!$el.parentElement || !$el.nextElementSibling) {
       return;
     }
+    const $sibling = $el.nextElementSibling;
+
     const $img = document.createElement('img');
     $img.src = this.url;
     $img.classList.add('card-image__image');
-    $el.parentElement.insertBefore($img, $el.nextSibling);
+
+    // Set margin dinamically because font size of <pre> is different on chrome and firefox for some reason.
+    $img.style.marginLeft = window.getComputedStyle($sibling).marginLeft;
+
+    $el.parentElement.insertBefore($img, $sibling);
   }
 }
